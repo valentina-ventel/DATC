acba76a0fdbaabab12285bcf77e44fde701ec114
Valentina <valentina.vali40@yahoo.com>
2019-11-12 17:18:32 +0200

    Initial Commit

---
+++ DATC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DATC
+//
+//  Created by Valentina Vențel on 12/11/2019.
+//  Copyright © 2019 Valentina Vențel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
