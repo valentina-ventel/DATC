11a7b5f0b175851a7be73c9f94e636a308595cb4
Valentina <valentina.vali40@yahoo.com>
2019-12-08 18:01:18 +0200

    Added .gitignore file

---
+++ DATC/FirebaseAuthManager.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseAuthManager.swift
+//  DATC
+//
+//  Created by Valentina Vențel on 30/11/2019.
+//  Copyright © 2019 Valentina Vențel. All rights reserved.
+//
+
+import UIKit
+
+class FirebaseAuthManager: NSObject {
+
+}
