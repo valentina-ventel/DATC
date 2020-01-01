ae4d0a58ca6e6ed44fa4939afc5443b554367a64
Valentina Vențel <valentina@Ciprians-MacBook-Pro.local>
2020-01-01 19:48:57 +0200

    Downloaded data

---
+++ DATC/Models/Report.swift
@@ -0,0 +1,13 @@
+//
+//  Report.swift
+//  DATC
+//
+//  Created by Valentina Vențel on 31/12/2019.
+//  Copyright © 2019 Valentina Vențel. All rights reserved.
+//
+
+import UIKit
+
+class Report: NSObject {
+
+}
---
+++ DATC/Models/ReportModelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ReportModelProtocol.swift
+//  DATC
+//
+//  Created by Valentina Vențel on 30/12/2019.
+//  Copyright © 2019 Valentina Vențel. All rights reserved.
+//
+
+import UIKit
+
+class ReportModelProtocol: NSObject {
+
+}
